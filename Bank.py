607b7c3f20823a5eb7b1a715ad0b7513eab0d28c
MeansMarkus <markus.adam2014@gmail.com>
2024-11-21 18:19:32 -0600

    added functions for account handling

--- Bank.py
+++ Bank.py
@@ -2,3 +2,23 @@ import tkinter as tk
 from tkinter import messagebox
 from time import gmtime, strftime
 
+#Handling of invalid credentials
+
+#function that us a combo try and catch to handle ValueError
+def is_number(s):
+    try:
+        float(s)
+        return 1
+    except ValueError:
+        return 0
+    #function to check whether acc number is valid or not
+def check_acc_nmb(num):
+    try: 
+        fpin=open(num + ".txt","r")
+    except FileNotFoundError:
+        messagebox.showinfo("Invalid Account Number", "Try Again")
+        return 0
+    fpin.close()
+    return
+
+    
